5e251f2cfc9f24354da506d85a598488197b9a6e
nattphenjati <nattphenjati@gmail.com>
2023-10-25 14:06:00 +0700

    Fix exam

--- kanji-prototype/flutter_service_worker.js
+++ kanji-prototype/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "525bd9aace4776da502d06a8dda03be6",
-"index.html": "78420d099897adf30507999dd501007f",
-"/": "78420d099897adf30507999dd501007f",
-"main.dart.js": "78360456cf6c45337d18a58280fc47b1",
+"index.html": "546715ac4b275bf81ab55bfc4bed8f88",
+"/": "546715ac4b275bf81ab55bfc4bed8f88",
+"main.dart.js": "31d8181ffdf55b5daae83f9b1459a961",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
