3e94f01ea2187c2f0136461e61b51dd58aa7371f
nattphenjati <nattphenjati@gmail.com>
2023-10-27 09:47:53 +0700

    Update Review

--- kanji-prototype/flutter_service_worker.js
+++ kanji-prototype/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "525bd9aace4776da502d06a8dda03be6",
-"index.html": "546715ac4b275bf81ab55bfc4bed8f88",
-"/": "546715ac4b275bf81ab55bfc4bed8f88",
-"main.dart.js": "31d8181ffdf55b5daae83f9b1459a961",
+"index.html": "c0b2bff067b18a085d983f1d39d1c109",
+"/": "c0b2bff067b18a085d983f1d39d1c109",
+"main.dart.js": "3805ab501ef4cf5b8e2edb1ebf5f0cf6",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
