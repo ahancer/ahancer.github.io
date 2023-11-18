b2fc4207f5fc6f98170467b2b898f876fe41e429
nattphenjati <nattphenjati@gmail.com>
2023-11-18 09:32:57 +0700

    UI theme

--- kanji-prototype/flutter_service_worker.js
+++ kanji-prototype/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "525bd9aace4776da502d06a8dda03be6",
-"index.html": "9a6a81e19d25b41bb3f9cf566dc701c6",
-"/": "9a6a81e19d25b41bb3f9cf566dc701c6",
-"main.dart.js": "4fdc3453d4041074fee262403aa7ca96",
+"index.html": "a7c981c7cc91f3b01c2420f3b218ed01",
+"/": "a7c981c7cc91f3b01c2420f3b218ed01",
+"main.dart.js": "23e7cd601cb1d14dac82eb00bbb281f5",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
