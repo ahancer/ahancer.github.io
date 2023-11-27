02ebe5856396dbd32f796f3c1f2232653fcfa1fc
nattphenjati <nattphenjati@gmail.com>
2023-11-27 16:55:10 +0700

    Fix Layout

--- kanji_dev_prototype/.dart_tool/flutter_build/8c581452690aa353dfab5131ce0776ec/main.dart
+++ kanji_dev_prototype/.dart_tool/flutter_build/8c581452690aa353dfab5131ce0776ec/main.dart
@@ -1,5 +1,5 @@
 // @dart=3.0
-// Flutter web bootstrap script for package:login_email/main.dart.
+// Flutter web bootstrap script for package:kanji_prototype/main.dart.
 //
 // Generated file. Do not edit.
 //
@@ -9,7 +9,7 @@
 import 'dart:ui_web' as ui_web;
 import 'dart:async';
 
-import 'package:login_email/main.dart' as entrypoint;
+import 'package:kanji_prototype/main.dart' as entrypoint;
 import 'web_plugin_registrant.dart' as pluginRegistrant;
 
 typedef _UnaryFunction = dynamic Function(List<String> args);
