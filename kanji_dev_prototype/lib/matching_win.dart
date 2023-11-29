6c809eedbd33f1167a7717466f3648416ac00df2
nanputcha <nan@ahancer.com>
2023-11-29 13:50:52 +0700

    Add UI - Matching lose & win

--- kanji_dev_prototype/lib/matching_win.dart
+++ kanji_dev_prototype/lib/matching_win.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kanji_prototype/app_styles.dart';
 import 'package:kanji_prototype/arcade.dart';
 
 class MatchingWin extends StatelessWidget {
@@ -7,33 +8,76 @@ class MatchingWin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('You Win!'),
-        automaticallyImplyLeading: false, // This will hide the back button
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.close),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Arcade()),
-              );
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: 
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Arcade()),
-              );
-            },
-            child: const Text('Back to Arcade', style: TextStyle(fontSize: 28),),
-          )
-        )
-      );
+        backgroundColor: Styles.bgGray0,
+        appBar: AppBar(
+          title: Text(''),
+          backgroundColor: Styles.bgGray0,
+          elevation: 0,
+          automaticallyImplyLeading: false, // This will hide the back button
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Arcade()),
+                );
+              },
+            )
+          ],
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Image(
+                image: AssetImage('assets/images/img-matching-win.png'),
+                alignment: Alignment.center,
+                width: double.infinity,
+                fit: BoxFit.fill,
+              ),
+              Expanded(
+                  child: Container(
+                color: Styles.bgWhite,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    SizedBox(height: 48),
+                    Text('Good Job!',
+                        style:
+                            Styles.H1.copyWith(color: Styles.textColorPrimary)),
+                    SizedBox(height: 12),
+                    Text(
+                      'Your word recall is impressive!',
+                      style: Styles.subTitle
+                          .copyWith(color: Styles.textColorPrimary),
+                    ),
+                    SizedBox(height: 80),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Styles.bgAccent,
+                        fixedSize: Size(220, 64),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              64.0), // Adjust the radius as needed
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Arcade()),
+                        );
+                      },
+                      child: Text(
+                        'Back to Arcade',
+                        style: Styles.textButton
+                            .copyWith(color: Styles.textColorPrimary),
+                      ),
+                    )
+                  ],
+                ),
+              )),
+            ],
+          ),
+        ));
   }
 }
