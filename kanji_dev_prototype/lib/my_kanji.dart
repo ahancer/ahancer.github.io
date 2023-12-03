41b8832880955ecdc37bac045f52ad256af605fe
nattphenjati <nattphenjati@gmail.com>
2023-12-03 18:00:18 +0700

    fix

--- kanji_dev_prototype/lib/my_kanji.dart
+++ kanji_dev_prototype/lib/my_kanji.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutterflow_ui/flutterflow_ui.dart';
 import 'package:intl/intl.dart';
 import 'package:kanji_prototype/app_data.dart';
 import 'package:kanji_prototype/app_styles.dart';
